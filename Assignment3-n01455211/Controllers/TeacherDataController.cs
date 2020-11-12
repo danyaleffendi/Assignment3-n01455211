7a5d62163420ff8edf4cc31b3bcb67b33fa5292a
danyaleffendi <danyal413@hotmail.com>
2020-11-12 01:18:11 -0500

    css customization

--- Assignment3-n01455211/Controllers/TeacherDataController.cs
+++ Assignment3-n01455211/Controllers/TeacherDataController.cs
@@ -11,6 +11,7 @@ namespace Assignment3_n01455211.Controllers
 {
     public class TeacherDataController : ApiController
     {
+        // This project is done with the help of instructor Christine Bittle's Blog Project.
         // The database context class which allows us to access our MySQL Database.
         private SchoolDbContext School = new SchoolDbContext();
 
@@ -25,52 +26,53 @@ namespace Assignment3_n01455211.Controllers
         [HttpGet]
         public IEnumerable<Teacher> TeacherInfo()
         {
-            //Create an instance of a connection
+            //Creating connection with database
             MySqlConnection Conn = School.AccessDatabase();
 
-            //Open the connection between the web server and database
+            //Acivate connection between the web server and database
             Conn.Open();
 
-            //Establish a new command (query) for our database
+            //Establish a new command for our database
             MySqlCommand cmd = Conn.CreateCommand();
 
-            //SQL QUERY
+            //SQL QUERY to access columns from teacher table
             cmd.CommandText = "Select * from teachers";
+            
 
-            //Gather Result Set of Query into a variable
+            //Incorporating SQL Query into a variable
             MySqlDataReader ResultSet = cmd.ExecuteReader();
 
-            //Create an empty list of Author Names
+            //Create an empty list of Teacher Names
             List<Teacher> Teachers = new List<Teacher> { };
 
             //Loop Through Each Row the Result Set
             while (ResultSet.Read())
             {
-                //Access Column information by the DB column name as an index
+                //Get column information by the column name from teacher table
                 int TeacherId = (int)ResultSet["teacherid"];
                 string TeacherFname = ResultSet["teacherfname"].ToString();
                 string TeacherLname = ResultSet["teacherlname"].ToString();
                 string EmployeeNumber = ResultSet["employeenumber"].ToString();
                 string HireDate = ResultSet["hiredate"].ToString();
                 string Salary = ResultSet["salary"].ToString();
-
+               
                 Teacher NewTeacher = new Teacher();
                 NewTeacher.TeacherId = TeacherId;
                 NewTeacher.TeacherFname = TeacherFname;
                 NewTeacher.TeacherLname = TeacherLname;
                 NewTeacher.EmployeeNumber = EmployeeNumber;
                 NewTeacher.HireDate = HireDate;
-                NewTeacher.Salary = Salary;
+                NewTeacher.Salary = Salary;               
 
-                //Add the Author Name to the List
+                //Adding Teacher Name to the List
                 Teachers.Add(NewTeacher);
             }
         
 
-            //Close the connection between the MySQL Database and the WebServer
+            //Ending connection between the MySQL Database and the WebServer
             Conn.Close();
 
-            //Return the final list of author names
+            //Returning the final list of teacher names
             return Teachers;
         }
         [HttpGet]
@@ -78,39 +80,39 @@ namespace Assignment3_n01455211.Controllers
         {
             Teacher NewTeacher = new Teacher();
 
-            //Create an instance of a connection
+            //Creating connection with database
             MySqlConnection Conn = School.AccessDatabase();
 
-            //Open the connection between the web server and database
+            //Acivate connection between the web server and database
             Conn.Open();
 
-            //Establish a new command (query) for our database
+            //Establish a new command for our database
             MySqlCommand cmd = Conn.CreateCommand();
 
-            //SQL QUERY
-            cmd.CommandText = "Select * from Teachers where Teacherid = " + id;            
+            //SQL QUERY to access columns from teacher table
+            cmd.CommandText = "Select * from Teachers where Teacherid = " + id;
 
-            //Gather Result Set of Query into a variable
+            //Incorporating SQL Query into a variable
             MySqlDataReader ResultSet = cmd.ExecuteReader();
 
             while (ResultSet.Read())
             {
-                //Access Column information by the DB column name as an index
+                //Get column information by the column name from teacher table
                 int TeacherId = (int)ResultSet["teacherid"];
                 string TeacherFname = ResultSet["teacherfname"].ToString();
                 string TeacherLname = ResultSet["teacherlname"].ToString();
                 string EmployeeNumber = ResultSet["employeenumber"].ToString();
                 string HireDate = ResultSet["hiredate"].ToString();
-                string Salary = ResultSet["salary"].ToString();
-                
+                string Salary = ResultSet["salary"].ToString();                
+
                 NewTeacher.TeacherId = TeacherId;
                 NewTeacher.TeacherFname = TeacherFname;
                 NewTeacher.TeacherLname = TeacherLname;
                 NewTeacher.EmployeeNumber = EmployeeNumber;
                 NewTeacher.HireDate = HireDate;
                 NewTeacher.Salary = Salary;
+                
             }
-
 
             return NewTeacher;
         }
--- Assignment3-n01455211/Models/Teacher.cs
+++ Assignment3-n01455211/Models/Teacher.cs
@@ -6,14 +6,13 @@ using System.Web;
 namespace Assignment3_n01455211.Models
 {
     public class Teacher
-    {
-        //The following fields define an Teacher
+    {        
+        //Columns from the Teacher table of our database
         public int TeacherId;
         public string TeacherFname;
         public string TeacherLname;
         public string EmployeeNumber;
         public string HireDate;
-        public string Salary;
-        public string Classname;
+        public string Salary;        
     }
 }
